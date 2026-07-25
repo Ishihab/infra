b2a99414067883e186240f0d1f2c91b9f3ace07b
Sohrab Hossain <sohrab@sohrabhossain.com>
2026-07-25 21:16:07 +0800

    Added k8s,helm providers and argocd helm chart
    - Added necessary providers to deploy argocd helm chart.
    - Added argocd helm chart to addons module.
    - set necessary chart values for argocd helm chart.

--- addons/providers.tf
+++ addons/providers.tf
@@ -2,6 +2,16 @@ provider "aws" {
   region = var.aws_region
 }
 
+provider "kubernetes" {
+  host                   = data.aws_ssm_parameter.cluster_endpoint.value
+  cluster_ca_certificate = base64decode(data.aws_ssm_parameter.ca_cert.value)
+  exec {
+    api_version = "client.authentication.k8s.io/v1"
+    args        = ["eks", "get-token", "--cluster-name", data.aws_ssm_parameter.cluster_name.value]
+    command     = "aws"
+  }
+}
+
 provider "helm" {
   kubernetes = {
     host                   = data.aws_ssm_parameter.cluster_endpoint.value
