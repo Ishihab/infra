33feef59255c5728b4b4973eed76e739b2547815
Sohrab Hossain <sohrab@sohrabhossain.com>
2026-08-03 09:57:26 +0800

    code format

--- eks/main.tf
+++ eks/main.tf
@@ -66,9 +66,9 @@ module "eks" {
       }
 
 
-      min_size              = 1
-      max_size              = 3
-      desired_size          = 2
+      min_size     = 1
+      max_size     = 3
+      desired_size = 2
     }
   }
 
