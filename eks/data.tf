278d8ab7b68b0429e294d26756864e9d5e699c47
Sohrab Hossain <sohrab@sohrabhossain.com>
2026-08-14 12:54:26 +0800

    Add pod identity for simple social app

--- eks/data.tf
+++ eks/data.tf
@@ -14,3 +14,19 @@ data "aws_kms_alias" "secrets_manager_kms_key_arn" {
 data "aws_ssm_parameter" "eci_endpoint_sg_id" {
   name = "/simple_social/${var.environment}/vpc/eci_endpoint_sg_id"
 }
+
+data "aws_iam_policy_document" "s3_access" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "s3:GetObject",
+      "s3:PutObject",
+      "s3:DeleteObject",
+      "s3:ListBucket"
+    ]
+    resources = [
+      "arn:aws:s3:::simple-social",
+      "arn:aws:s3:::simple-social/*"
+    ]
+  }
+}
